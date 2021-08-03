0592ff37d3d29a29e99ce4f734968527ad89a474
Ya-Chieh Lai <yachieh@gmail.com>
2021-08-02 23:51:34 -0700

    Initial Commit

---
+++ My Sudoku Solver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Sudoku Solver
+//
+//  Created by Sabrina Hsieh on 8/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Sudoku Solver/My_Sudoku_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Sudoku_SolverApp.swift
+//  My Sudoku Solver
+//
+//  Created by Sabrina Hsieh on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct My_Sudoku_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
